6aa3fef76d426a2ea7f078bfce50ebd07f0ea7a5
Adailson Santos <adailson2@gmail.com>
2020-02-19 18:45:18 -0300

    styling app.js in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import api from './services/api';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    console.log('hello world');
+  }
+
+  return (
+    <div className="container">
+       <img src={ logo } alt="AirCnc"/>
+
+       <div className="content">
+         <p>
+           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+         </p>
+         <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+           type="email"
+           id="email" 
+           placeholder="Seu melhor e-mail" 
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+         </form>
+       </div>
+    </div>
+  );
+}
+
+export default App;
