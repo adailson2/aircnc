7513c63899ea31750f4c653a6ee77bb78cdecedc
Adailson Santos <adailson2@gmail.com>
2020-02-20 19:38:11 -0300

    create layout login for mobile

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, KeyboardAvoidingView, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login() {
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Image source={logo} />
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder="Seu e-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+      <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder="Tecnologias de interesse"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Encontrar spots</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+  label: {
+    fontWeight: 'bold',
+    color:'#444',
+    marginBottom: 8, 
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize:16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 4,
+  },
+  button: {
+    width: 300,
+    height: 50,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
+})
