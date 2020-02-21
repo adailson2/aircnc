37d8532ea44bdb8d734fc1bcac2b1720419a3688
Adailson Santos <adailson2@gmail.com>
2020-02-20 22:34:10 -0300

    implement book to choice spot for booking

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,6 +1,91 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Alert, SafeAreaView, TextInput, TouchableOpacity, AsyncStorage, StyleSheet } from 'react-native';
 
-export default function Book() {
-  return <View />
+import api from '../services/api';
+
+export default function Book({ navigation }) {
+  const [date, setDate] = useState('');
+  const id = navigation.getParam('id');
+
+  async function handleSubmit(){
+    const user_id = await AsyncStorage.getItem('user');
+
+    await api.post(`/spots/${id}/bookings`, {
+      date
+    }, { 
+      headers: { user_id }
+    })
+
+    Alert.alert('Solicitação de reserva enviada.');
+    navigation.navigate('List');
+  }
+
+  async function handleCancel(){
+    navigation.navigate('List');
+  }
+  
+  return (
+    <SafeAreaView style={styles.container} >
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput 
+          style={styles.input}
+          placeholder="Qual data você quer reservar?"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={date}
+          onChangeText={setDate}
+        />
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar spots</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+    marginTop: 50
+  },
+  label: {
+    fontWeight: 'bold',
+    color:'#444',
+    marginBottom: 8, 
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize:16,
+    color: '#444',
+    height: 44,
+    width: 300,
+    marginBottom: 20,
+    borderRadius: 4,
+  },
+  button: {
+    width: 300,
+    height: 50,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  cancelButton: {
+    marginTop: 10,
+    backgroundColor: '#ccc',
+  },
+});
